758eaf575c7d804136b3016b9ba24d7d81449241
developer <developer>
2018-02-08 09:45:09 -0600

    Initial commit

---
+++ src/main/java/com/pojectforjpa/jpalab/JpalabApplication.java
@@ -0,0 +1,12 @@
+package com.pojectforjpa.jpalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpalabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpalabApplication.class, args);
+	}
+}
